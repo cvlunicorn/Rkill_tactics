65610b71b5dff1ff6c21260369e3466026aa0631
cvlunicorn <349520287@qq.com>
2025-09-14 10:13:43 +0800

    舰r战术0.4使用无名杀默认格式重置，移除美化包的图片与代码残留。现在有护航援护和炮击训练两张牌。

--- extension.js
+++ extension.js
@@ -1,29 +1,16 @@
+import { lib, game, ui, get, ai, _status } from "../../noname.js";
 game.import("extension", function (lib, game, ui, get, ai, _status) {
     return {
         name: "舰R战术", content: function (config, pack) {
-            /*if (config.jianrzs) {
-                lib.skill._shashanwuxietao = {
-                    prompt: "将♦牌当做杀，♥牌当做桃，♣牌当做闪，♠牌当做无懈可击使用或打出(bushi)，<br>将牌当作原版的牌使用或打出。<br>响应事件只能打出对应名字的卡牌，<br>想打出让其他牌响应事件，需要视为技的帮助",
-                    mod: {
-                        cardname: function (card, player, name) {
-                            if (card.name == 'sheji9') { return 'sha'; }; if (card.name == 'huibi9') { return 'shan'; }; if (card.name == 'zhikongquan9') { return 'wuxie'; }; if (card.name == 'kuaixiu9') { return 'tao'; }; if (card.name == 'zziqi9') { return 'jiu'; };
-                            if (card.name == 'juedouba9') { return 'juedou'; };
-                            if (card.name == 'leibusigang9') { return 'lebu'; }; if (card.name == 'nobuji9') { return 'bingliang'; }; if (card.name == 'taifeng9') { return 'shandian'; };
-                        },
-                    },
-                };
-                for (var i in lib.characterPack['jianrzs']) {
-                    if (lib.character[i][4].indexOf("forbidai") < 0) lib.character[i][4].push("forbidai");
-                };
-            };*/
-        }, precontent: function () {
+
+        }, precontent: function (jianrzsbao) {
             game.import('card', function () {
                 var jianrzsbao = {
                     name: 'jianrzsbao',//卡包命名
                     connect: true,//卡包是否可以联机
                     card: {
-                        "huhangyuanhu": {
-                            image: 'ext:舰R美化/ewaibuji9.png',
+                        huhangyuanhu9: {
+                            image: 'ext:舰R战术/huhangyuanhu9.png',
                             audio: true,
                             fullskin: true,
                             type: "trick",
@@ -64,8 +51,8 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                                 },
                             },
                         },
-                        "paojixunlian": {
-                            image: 'ext:舰R美化/bigseven9.png',
+                        paojixunlian9: {
+                            image: 'ext:舰R战术/paojixunlian9.png',
                             audio: true,
                             fullskin: true,
                             type: "trick",
@@ -135,48 +122,48 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                             },
                         },
 
-
                     },
                     skill: {
                         paojixunlian: {
                             charlotte: true,
                             locked: true,
+                            mark: true,
                             intro: {
                                 content: function (storage, player, skill) {
-                                    return '使用杀的次数+' + get.translation(player.storage.paojixunlian * 2);
+                                    return '出杀次数' + get.translation(player.storage.paojixunlian * 2);
                                 },
                             },
                             mod: {
                                 cardUsable: function (card, player, num) {
-                                    if (card.name == 'sha') return num + 2 * player.storage.paojixunlian;
+                                    if (card.name == 'sha') return num + player.storage.paojixunlian * 2;
                                 },
-                            },
-                            "_priority": 0,
-                        }
+                                "_priority": 0,
+                            }
+
+                        },
                     },
                     translate: {
-                        "huhangyuanhu": "护航援护",
-                        "huhangyuanhu_info": "出牌阶段，对一名其他角色使用。其摸两张牌。",
-                        "paojixunlian": "炮击训练",
-                        "paojixunlian_info": "出牌阶段，对自己使用。本回合你使用杀的上限+2。",
+                        "huhangyuanhu9": "护航援护",
+                        "huhangyuanhu9_info": "出牌阶段，对一名其他角色使用。其摸两张牌。",
+                        "paojixunlian9": "炮击训练",
+                        "paojixunlian9_info": "出牌阶段，对自己使用。本回合你使用杀的上限+2。",
 
                     },
                     list: [
-                        ["heart", 10, "huhangyuanhu"],
-                        ["heart", 11, "huhangyuanhu"],
-                        ["heart", 12, "huhangyuanhu"],
-                        ["club", 10, "paojixunlian"],
-                        ["club", 11, "paojixunlian"],
-                        ["club", 12, "paojixunlian"],
+                        ["heart", 10, "huhangyuanhu9"],
+                        ["heart", 11, "huhangyuanhu9"],
+                        ["heart", 12, "huhangyuanhu9"],
+                        ["club", 10, "paojixunlian9"],
+                        ["club", 11, "paojixunlian9"],
+                        ["club", 12, "paojixunlian9"],
                     ],//牌堆添加
-                }
-                return jianrzsbao
+                };
+
+                return jianrzsbao;
             });
             lib.translate['jianrzsbao_card_config'] = '舰R战术卡包';
             lib.config.all.cards.push('jianrzsbao');
             if (!lib.config.cards.contains('jianrzsbao')) lib.config.cards.push('jianrzsbao');//包名翻译
-
-            //选项触发内容，原因见config
         }, help: {}, config: {}, package: {
             character: {
                 character: {
@@ -192,13 +179,16 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                 list: [],
             },
             skill: {
-
+                skill: {
+                },
+                translate: {
+                },
             },
             intro: "",
-            author: "※无名玩家",
+            author: "无名玩家",
             diskURL: "",
             forumURL: "",
             version: "0.1",
-        }, files: { "character": [], "card": [], "skill": [] }
+        }, files: { "character": [], "card": [], "skill": [], "audio": [] }
     }
-})
+});
